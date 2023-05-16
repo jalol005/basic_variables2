54247d80d681482ffaba3c34cd72a913ce2798b7
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-16 15:41:25 +0500

    09

--- bin/basic_variables209.dart
+++ bin/basic_variables209.dart
@@ -1,1 +1,5 @@
 // Create a variable called "isWeekend" of type boolean and assign true or false based on whether it is currently the weekend or not.
+void main(){
+bool isWeekend = false;
+print(isWeekend);
+}
