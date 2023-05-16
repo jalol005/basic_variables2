2ccd5d41d3ad1dcbf2b1a903c23aa4294df2f49a
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-16 15:38:48 +0500

    02

--- bin/basic_variables202.dart
+++ bin/basic_variables202.dart
@@ -1,1 +1,5 @@
 // Declare a variable called "temperature" of type double and assign the current temperature in Celsius as the value.
+void main(){
+    double temperature = 24.0;
+    print(temperature);
+}
