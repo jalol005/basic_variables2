fcd07a6331a1edcec245e855cb0d8a72e52e82fa
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-16 15:41:50 +0500

    10

--- bin/basic_variables210.dart
+++ bin/basic_variables210.dart
@@ -1,1 +1,5 @@
 // Declare a variable called "bankBalance" of type double and assign the amount of money in your bank account as the value.
+void main(){
+    double bankBalance = 34.5
+    print(bankBalance);
+}
