92d7f04540b9e48af29de2e79dd5e9f87df21804
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-16 15:39:04 +0500

    03

--- bin/basic_variables203.dart
+++ bin/basic_variables203.dart
@@ -1,1 +1,5 @@
 //Create a variable called "favoriteBook" of type String and assign the title of your favorite book as the value.
+void main(){
+String favoriteBook = 'otkan kunlar';
+print(favoriteBook);
+}
